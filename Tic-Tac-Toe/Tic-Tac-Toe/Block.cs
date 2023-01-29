5c3b7a30e6bb7ba0cb27f51c01f75d7bf41891e8
Himwong113 <107347826+Himwong113@users.noreply.github.com>
2023-01-29 17:22:58 +0800

    Add files via upload

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/Block.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe 
+{
+    class Block
+    {
+        public static int userinput;
+        public static int round;
+        public static int row;
+        public static int col;
+        public static char[] OXarr = { '1','2','3','4','5','6','7','8','9'};
+
+
+
+
+
+        public static bool Valid(int userinput, int[,] Blockconfig)
+        {
+
+            if (userinput >= 1 && userinput <= 9)
+            {
+                userinput -= 1;
+                row = userinput / 3;
+                col = userinput % 3;
+                if (Blockconfig[row, col] == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public static int[,] assign(int userinput, int[,] Blockconfig, int round)
+        {
+            if (round % 2 == 0)
+            {
+                Blockconfig[row, col] = 1;
+            }
+            else
+            {
+                Blockconfig[row, col] = 2;
+            }
+
+
+            int k;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                   k = 3*i + j;
+                    if (Blockconfig[i, j] == 1)
+                    {
+
+                        OXarr[k] = 'O';
+                    }
+                    else if (Blockconfig[i, j] == 2)
+                    {
+                        OXarr[k] = 'X';
+                    }
+
+
+
+                }
+            }
+
+            return Blockconfig;
+
+
+        }
+
+
+
+        public static void Show()
+        {
+
+          
+
+
+
+            Console.WriteLine(
+                        "\t" + OXarr[0] + "     ||" + OXarr[1] + "     ||"+ OXarr[2] + "\n"+ 
+                        "\t====================\n" +
+                      "\t" + OXarr[3] + "     ||" + OXarr[4] + "     ||" + OXarr[5] + "\n" +
+                        "\t====================\n" +
+                     "\t" + OXarr[6] + "     ||" + OXarr[7] + "     ||" + OXarr[8] + "\n" 
+
+
+                    );
+
+        }
+
+        public static bool End(int[,] Blockconfig)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+
+                    if (Blockconfig[i, j] == 0)
+                    {
+                        return true;
+
+
+                    }
+
+
+                }
+            }
+
+            return false;
+
+        }
+
+    }
+
+}
---
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -0,0 +1,100 @@
+﻿
+
+using System.Data;
+using System.Threading.Channels;
+using Tic_Tac_Toe;
+
+int[,] Blockconfig = new int[3, 3];
+int round = 0;
+int userinput;
+
+while (Block.End(Blockconfig))
+{
+    Block.Show();
+    Console.WriteLine("Round :"+(round+1).ToString());
+   
+        Console.WriteLine("User "+ Convert.ToString(round%2+1)+" ,please select cell");
+        
+    
+   
+    userinput = Convert.ToInt32(Console.ReadLine());
+    Block.userinput = userinput;
+
+    if (Block.Valid(userinput, Blockconfig))
+    {
+        Blockconfig = Block.assign(userinput, Blockconfig,round);
+
+
+      
+
+
+    }
+
+
+    else
+    {
+        Console.WriteLine("Not valid! Please check input!");
+        Console.ReadLine();
+        round--;
+    }
+
+    
+
+    
+    Console.Clear();
+    round++;
+}
+
+
+
+Block.Show();
+int buff=0;
+int buff2 = 0;
+int buff3 = 0;
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+       
+
+        buff+= Blockconfig[i, j];
+        buff2 += Blockconfig[i, j];
+        if (i == j)
+        {
+            buff3 += Blockconfig[i, j];
+        }
+        
+    }
+
+    
+    
+    
+}
+
+int buff4 = Blockconfig[0, 2]+ Blockconfig[1, 1]+Blockconfig[2, 0] ;
+
+
+if (buff == 3 || buff2 == 3 || buff3 == 3|| buff4 == 3)
+{
+    Console.WriteLine("Winner :User1");
+   
+}
+else if (buff == 6 || buff2 == 6 || buff3 == 6|| buff4 == 6)
+{
+    Console.WriteLine("Winner :User2");
+   
+}
+
+
+
+
+
+
+if (buff != 3 && buff2 != 3 && buff3 != 3 && buff != 6 && buff2 != 6 && buff3 != 6 && buff4  != 6)
+{
+    Console.WriteLine("Ddraw");
+}
+
+
+//Block.Show();
